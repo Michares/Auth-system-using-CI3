204a7e3e270fd5d68b27e8e01efe23048f1f5682
Michares <michaelariestian@gmail.com>
2026-08-04 20:56:57 +0700

    add update profile features

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,7 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
-    #[Override]
     public function __construct()
     {
         parent::__construct();
@@ -12,6 +11,9 @@ class Auth extends CI_Controller
 
     public function index()
     {
+        if ($this->session->userdata('email')){
+            redirect('users');
+        }
         $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
         $this->form_validation->set_rules('password', 'Password', 'required|trim');
         if ($this->form_validation->run() == false) {
@@ -73,6 +75,9 @@ class Auth extends CI_Controller
 
     public function registration()
     {
+        if ($this->session->userdata('email')){
+            redirect('users');
+        }
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
         $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]',[
             'is_unique' => "This Email has already registered!"
--- application/views/user/index.php
+++ application/views/user/index.php
@@ -5,8 +5,14 @@
                     <!-- Page Heading -->
                     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
+                    <div class="row">
+                        <div class="col-lg-8">
+                            <?= $this->session->flashdata('message'); ?>
+                        </div>
+                    </div>
+
                     <!-- Profile Card -->
-                    <div class="card mb-3" style="max-width: 540px;">
+                    <div class="card mb-3 col-lg-8">
                     <div class="row no-gutters">
                         <div class="col-md-4">
                         <img src="<?= base_url('assets/img/profile/'). $user['image']; ?>" class="card-img" alt="...">
